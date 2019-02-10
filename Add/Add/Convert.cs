488b41ea1c27ef6dd79bd5a2b4efb0e5dc7990c1
Chazz6000 <Charlie-devine@hotmail.co.uk>
2019-02-10 22:17:05 +0000

    Add files via upload
    Simple app that adds to numbers. Created this for a work colleague to show how complex it can get.

---
+++ Add/Add/Convert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add
+{
+    class Convert
+    {
+
+        private  int _num1 { get; set; }
+        private int _num2 { get; set; }
+        private int _result { get; set; }
+
+        public Convert(int num1, int num2)
+        {
+            _num1 = num1;
+            _num2 = num2;
+
+            _result = _num1 + _num2;
+
+            Console.WriteLine("Your total is " + _result);
+
+        }
+
+        
+              
+    }
+
+
+    
+}
---
+++ Add/Add/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter first number ");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter Second number ");
+            int num2 = Int32.Parse(Console.ReadLine());
+
+            Convert convert = new Convert(num1, num2);
+        
+
+            
+            
+        }
+    }
+}
